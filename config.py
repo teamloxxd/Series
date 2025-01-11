dc3b3f029c85532c9c51215c6ce5e585a7e9522b
LOXXD <sijithappu7902@gmail.com>
2025-01-11 17:23:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -16,15 +16,15 @@ class Config(object):
     
     
     # Database URL from https://cloud.mongodb.com/
-    DATABASE_URI = os.environ.get("DATABASE_URI", "")
+    DATABASE_URI = os.environ.get("DATABASE_URI", "mongodb+srv://mcadzzseries:mcadzzseries@cluster0.n6gi7wa.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
 
     # Your database name from mongoDB
-    DATABASE_NAME = str(os.environ.get("DATABASE_NAME", "Cluster0"))
+    DATABASE_NAME = str(os.environ.get("DATABASE_NAME", "mcadzzseries"))
 
 
     # ID of users that can use the bot commands
-    AUTH_USERS = set(str(x) for x in os.environ.get("AUTH_USERS", "").split())
+    AUTH_USERS = set(str(x) for x in os.environ.get("AUTH_USERS", "684727861").split())
 
 
     # To save user details (Usefull for getting userinfo and total user counts)
